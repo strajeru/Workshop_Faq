16877833d218a6534cc90fe0ba01014c18882b0e
Marius Strajeru <marius.strajeru@gmail.com>
2017-03-08 14:07:21 +0200

    fix mass actions

---
+++ Controller/Adminhtml/Faq/MassDisable.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Workshop_Faq extension
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * @category  Workshop
+ * @package   Workshop_Faq
+ * @copyright Copyright (c) 2017
+ * @license   http://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Workshop\Faq\Controller\Adminhtml\Faq;
+
+use Workshop\Faq\Api\Data\FaqInterface;
+use Workshop\Faq\Controller\Adminhtml\Faq\MassAction;
+use Workshop\Faq\Model\Faq;
+
+class MassDisable extends MassAction
+{
+    /**
+     * @param \Workshop\Faq\Api\Data\FaqInterface $faq
+     * @return $this
+     */
+    protected function massAction(FaqInterface $faq)
+    {
+        $faq->setIsActive(Faq::STATUS_DISABLED);
+        $this->faqRepository->save($faq);
+        return $this;
+    }
+}
